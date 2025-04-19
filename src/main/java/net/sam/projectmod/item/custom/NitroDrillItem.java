e1dc81ab51a4486d8dd733ebab20e66a0e4a30fc
CorKaboom <samuelrollins05@gmail.com>
2025-04-18 17:16:18 -0700

    Nitro Drill files

---
+++ src/main/java/net/sam/projectmod/item/custom/NitroDrillItem.java
@@ -0,0 +1,9 @@
+package net.sam.projectmod.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class NitroDrillItem extends Item {
+    public NitroDrillItem(Properties pProperties){
+        super(pProperties);
+    }
+}
